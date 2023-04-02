bca8659d2df1794307939094bc828dc92dd07d25
Dom <s24500@pjwstk.edu.pl>
2023-04-02 13:57:16 +0200

    :))

--- ppy4/zad3.py
+++ ppy4/zad3.py
@@ -1,13 +1,20 @@
 def metoda(key, number):
+    if number > 31 :
+        return "Wprowadzono błędne dane"
     array = list(key)
     result = ""
     for znak in array:
         if znak.isalpha():
             x = ord(znak)
-            x = x + number
+            if x < 97 :
+                x=x+32
+            if x+number>122 :
+                x=x+number-26
+            else:
+                x=x+number
             znak = chr(x)
         result = result + znak
     return result
 
 
-print(metoda("", 5))
+print(metoda("zAbyW", 3))
